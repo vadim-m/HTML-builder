e48bf79021e6949ddf12b54f148074a48c93ca41
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2022-11-08 01:26:41 +0300

    refactor: task 03 via promise

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,23 +1,18 @@
 const path = require("path");
-const fs = require("fs");
 const { readdir } = require("fs/promises");
+const { stat } = require("fs/promises");
 
 readdir(path.join(__dirname, "secret-folder"), { withFileTypes: true }).then(
   (files) => {
     for (const file of files) {
       if (file.isFile()) {
-        showInfo(file);
+        stat(path.join(__dirname, "secret-folder", file.name)).then((item) => {
+          const info = file.name.split(".");
+          console.log(
+            `${info[0]} - ${info[1]} - ${(item.size / 1024).toFixed(3)}kb`
+          );
+        });
       }
     }
   }
 );
-
-function showInfo(file) {
-  fs.stat(path.join(__dirname, "secret-folder", file.name), (err, stats) => {
-    if (err) throw err;
-    const info = file.name.split(".");
-    console.log(
-      `${info[0]} - ${info[1]} - ${(stats.size / 1024).toFixed(2)}kb`
-    );
-  });
-}
