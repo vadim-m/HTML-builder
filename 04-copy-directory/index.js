6b521133711d2c8f9b5a0e3592fa7173f23e5083
Vadim Monastyrskiy <vmonastyrskiy@yandex.ru>
2022-11-08 17:56:38 +0300

    feat: solve task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,26 @@
+const path = require("path");
+const { mkdir, readdir, copyFile } = require("fs/promises");
 
+copyDir();
+
+async function copyDir() {
+  try {
+    const filesInFolder = await readdir(path.join(__dirname, "files"));
+    const createNewDir = await mkdir(path.join(__dirname, "files-copy"), {
+      recursive: true,
+    });
+
+    filesInFolder.forEach((file) => {
+      copyFileToNewDirectory(file);
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+async function copyFileToNewDirectory(file) {
+  await copyFile(
+    path.join(__dirname, "files", file),
+    path.join(__dirname, "files-copy", file)
+  );
+}
